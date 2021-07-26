ba67b221293574e7776217082e0aa6cf74c82577
ankani <ankanichattoraj91@gmail.com>
2021-07-25 20:56:25 -0400

    Subjectwise analysis files segregated wrt dataset type

--- ExperimentCode/newExperimentStimComputation.m
+++ ExperimentCode/newExperimentStimComputation.m
@@ -2,7 +2,7 @@
 screen_width_cm = 60;
 screen_width_pixel = 1920;
 pixels_per_cm = 1920/60;
-dist_to_screen_cm = 109.2;
+dist_to_screen_cm = 102.5;%case1:102,sf?;
 dia_deg_out_small = 2.088;
 dia_deg_in_small = 0.4352;
 pixels_out = ceil(tand(dia_deg_out_small) * dist_to_screen_cm * pixels_per_cm);
@@ -11,10 +11,10 @@ pixels_in = ceil(tand(dia_deg_in_small) * dist_to_screen_cm * pixels_per_cm);
 %compute area in cortex for given pixel size of stimulus
 constant1 = 0.0574;
 constant2 = 0.0897;
-times_old_outer = 4;
+times_old_outer = 1%%case2:3.6;
 old_out_mm = 8.9164;
 old_in_mm = 2.2712;
-monitor_dist = 109.2;
+monitor_dist = dist_to_screen_cm;%109.2;
 old_outer_radius_pix = pixels_out/2;
 r_mm = sqrt((times_old_outer^2+1) * old_out_mm^2 - old_in_mm^2);
 r_deg_out = ((exp(r_mm * constant1) * constant2) - constant2)/constant1;
