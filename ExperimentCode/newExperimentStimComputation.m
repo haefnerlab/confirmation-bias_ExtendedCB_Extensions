ef506457ad6a4e9c4eb73840d25a3818bea5f8d6
ankani <ankanichattoraj91@gmail.com>
2021-07-25 21:01:11 -0400

    small changes

--- ExperimentCode/newExperimentStimComputation.m
+++ ExperimentCode/newExperimentStimComputation.m
@@ -1,8 +1,8 @@
 %compute pixels size of stimulus based on visual degrees of stimulus 
 screen_width_cm = 60;
 screen_width_pixel = 1920;
-pixels_per_cm = 1920/60;
-dist_to_screen_cm = 109.2;
+pixels_per_cm = 1920/53.34;%jude's lab    1920/60; %our lab
+dist_to_screen_cm = 91.4;%jude's lab  109.2;% our lab
 dia_deg_out_small = 2.088;
 dia_deg_in_small = 0.4352;
 pixels_out = ceil(tand(dia_deg_out_small) * dist_to_screen_cm * pixels_per_cm);
@@ -11,10 +11,10 @@ pixels_in = ceil(tand(dia_deg_in_small) * dist_to_screen_cm * pixels_per_cm);
 %compute area in cortex for given pixel size of stimulus
 constant1 = 0.0574;
 constant2 = 0.0897;
-times_old_outer = 4;
+times_old_outer = 3.6;
 old_out_mm = 8.9164;
 old_in_mm = 2.2712;
-monitor_dist = 109.2;
+monitor_dist = dist_to_screen_cm;%91.4;%109.2;
 old_outer_radius_pix = pixels_out/2;
 r_mm = sqrt((times_old_outer^2+1) * old_out_mm^2 - old_in_mm^2);
 r_deg_out = ((exp(r_mm * constant1) * constant2) - constant2)/constant1;
